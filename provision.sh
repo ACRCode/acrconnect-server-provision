388f0fade84b7c5ae6b550798138969a8c4f3752
kglassmire <kglassmire@gmail.com>
2020-03-05 16:55:24 -0500

    add main script, install desktop and xrdp

---
+++ provision.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo bash ./scripts/install-ubuntu-desktop.sh
+sudo bash ./scripts/install-xrdp.sh -g yes -s yes
+
+
---
+++ scripts/install-ubuntu-desktop.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt install tasksel -y
+sudo tasksel install ubuntu-desktop
