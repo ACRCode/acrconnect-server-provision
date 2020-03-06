acc02adc1c293cd3ab7958f0f5a05329151a1dd4
kglassmire <kglassmire@gmail.com>
2020-03-06 14:11:12 -0500

    making more generic

--- aws-onlaunch.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/acrcode/acrconnect-aws-provision.git
-cd acrconnect-aws-provision
-sudo bash provision.sh
---
+++ onlaunch.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/acrcode/acrconnect-server-provision.git
+cd acrconnect-server-provision
+sudo bash provision.sh
